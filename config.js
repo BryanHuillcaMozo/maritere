25ab902867ab20dcd4a94571253fe7fd5cac590b
BryanHuillcaMozo <72517907+BryanHuillcaMozo@users.noreply.github.com>
2024-02-14 08:46:29 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+
+
+var config = {
+    
+    texts: [
+        "HOLA HONGUITO! :)",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "RECUERDA!",  // 同上...
+        "ERES LO MAS BONITO QUE TENGO",
+        "LA PERSONITA QUE ALEGRA ",
+        "MI DIA A DIA",
+        "<(^_^)>",
+        "ME GUSTA VERTE SIEMPRE SONREIR",
+        "NUNCA CAMBIES",
+        "TE QUIERO MUCHO <3",
+        "...",
+        "BRYAN ~~",
+    ],
+    wish: "FELIZ MIERCOLES 14 ^_^",  //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+    // 时间的格式很重要哦，一定要是下面这个格式!!
+    // 年年年年-月月-日日 时时:分分:秒秒
+    //time: "2017-07-20 0:0:0",    //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+};
